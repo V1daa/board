413fb8ea5d5e8cb13abcb01108a57a8286a75732
V1daa <mrjokerekoj@gmail.com>
2024-02-26 18:03:24 +0100

    Initialize

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DashboardPage = () => {
+  return (
+    <div>
+      DashboardRootPage
+    </div>
+  )
+}
+
+export default DashboardPage
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://stirring-cub-29.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
