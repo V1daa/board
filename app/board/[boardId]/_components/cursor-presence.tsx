15446b714c4f36e63bdbedeba0b997eab16ab026
V1daa <mrjokerekoj@gmail.com>
2024-03-03 15:10:29 +0100

    Version finalle?

--- app/board/[boardId]/_components/cursor-presence.tsx
+++ app/board/[boardId]/_components/cursor-presence.tsx
@@ -1,29 +1,58 @@
-'use client'
+"use client";
 
-import { useOthersConnectionIds } from '@/liveblocks.config'
-import {memo} from 'react'
-import { Cursor } from './cursor';
+import { useOthersConnectionIds, useOthersMapped } from "@/liveblocks.config";
+import { memo } from "react";
+import { Cursor } from "./cursor";
+import { shallow } from "@liveblocks/client";
+import { Path } from "./path";
+import { colorToCss } from "@/lib/utils";
 const Cursors = () => {
-    const ids = useOthersConnectionIds();
+  const ids = useOthersConnectionIds();
 
-    return(
-        <>
-        {ids.map((connectionId) => (
-            <Cursor 
-            key={connectionId}
-            connectionId={connectionId}
+  return (
+    <>
+      {ids.map((connectionId) => (
+        <Cursor key={connectionId} connectionId={connectionId} />
+      ))}
+    </>
+  );
+};
+
+const Draftx = () => {
+  const others = useOthersMapped(
+    (other) => ({
+      pencilDraft: other.presence.pencilDraft,
+      penColor: other.presence.penColor,
+    }),
+    shallow
+  );
+
+  return (
+    <>
+      {others.map(([key, other]) => {
+        if (other.pencilDraft) {
+          return (
+            <Path
+              key={key}
+              x={0}
+              y={0}
+              points={other.pencilDraft}
+              fill={other.penColor ? colorToCss(other.penColor) : "#101"}
             />
-        ))}
-        </>
-    )
-}
+          );
+        }
+      })}
+    </>
+  );
+};
 
 export const CursorPresence = memo(() => {
-    return(
-        <>
-        <Cursors />
-        </>
-    )
-})
+  return (
+    <>
+    <Draftx />
+      <Cursors />
+    </>
+  );
+});
 
-CursorPresence.displayName = 'CursorPresence'
+CursorPresence.displayName = "CursorPresence";
